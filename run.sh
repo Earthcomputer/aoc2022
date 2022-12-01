638da587d848234d9a74a46c6678ca88252e2e17
Joe <burtonjae@hotmail.co.uk>
2022-12-01 19:05:35 +0000

    Move code to src folder

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
-for i in *.asm; do
-  nasm -g -F dwarf -felf64 $i -o target/objects/${i%.asm}.o
+[ -d target/objects ] && find target/objects -name '*.o' -exec rm {} \;
+[ -f target/aoc2022 ] && rm target/aoc2022
+
+for i in $(find src -name '*.asm'); do
+  rel=$(realpath --relative-to=src/ $i)
+  mkdir -p $(dirname target/objects/$rel)
+  nasm -g -F dwarf -felf64 $i -o target/objects/${rel%.asm}.o
 done
 
-ld target/objects/*.o -o target/aoc2022
+ld $(find target/objects -name '*.o') -o target/aoc2022
 
 target/aoc2022
